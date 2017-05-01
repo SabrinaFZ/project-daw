0ec6988d0ca5080ec280c83191c6a35395e064a6
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-05-01 14:33:49 +0200

    changes stuff in admin and home and security

--- src/main/java/org/sabDav/configuration/SecurityConfiguration.java
+++ src/main/java/org/sabDav/configuration/SecurityConfiguration.java
@@ -1,15 +1,11 @@
 package org.sabDav.configuration;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(securedEnabled = true)
@@ -40,9 +36,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().anyRequest().authenticated();
 		http.formLogin().loginPage("/login").usernameParameter("username")
 		.passwordParameter("password").successHandler(customSuccessHandler)
-		.failureUrl("/login?error").permitAll().and().csrf();
+		.failureUrl("/login?error").permitAll();
 		http.logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout")
 		.permitAll();
+		http.csrf().disable();
 	}
 	
 }
