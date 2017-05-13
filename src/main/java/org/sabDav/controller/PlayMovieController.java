cdb0fb9f2de58b3617bfd3ba870938759d94f0b6
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-05-13 15:29:50 +0200

    add custom movie page

--- src/main/java/org/sabDav/controller/PlayMovieController.java
+++ src/main/java/org/sabDav/controller/PlayMovieController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -18,23 +19,18 @@ public class PlayMovieController {
 	@Autowired
 	private MovieService movieService;
 
-	@RequestMapping("/play/{id}")
+	@RequestMapping(value="/play/{id}", method = RequestMethod.GET)
     public ModelAndView playMovie(Model model, @PathVariable int id) throws MalformedURLException {
-
-		//TODO Comprobar si usuario esta logeado
 		
 		ModelAndView modelAndView = new ModelAndView();
 		MovieModel movie = movieService.findById(id);
-		if(movie == null){
-			modelAndView.setViewName("/components/movie/search");
-		}else{
-			URL url = new URL(movie.getUrl_movie());
-			modelAndView.addObject("successMessage", "Playing movie.");
-			model.addAttribute("movie", movie);
-			model.addAttribute("url", url);
-			modelAndView.setViewName("/components/movie/play");
-		}		
+		modelAndView.addObject("searchMovie", new MovieModel());
+		modelAndView.addObject("movie", movie);
+		modelAndView.setViewName("/components/user/play");
+		
+		
 		return modelAndView;	
 	}//playMovie
+
 	
 }//class
