2e37cdc786507393c4cb640cb3f705d011304e89
David Ortega Arcos <w78david@hotmail.com>
2017-05-22 16:52:16 +0200

    Versión preliminar de la nueva pantalla de búsqueda.
    Implementada en "advanced search", accesible mediante /search/advanced.
    Sustituirá a /search en el próximo commit.

--- src/main/java/org/sabDav/controller/SearchMovieController.java
+++ src/main/java/org/sabDav/controller/SearchMovieController.java
@@ -154,6 +154,49 @@ public class SearchMovieController {
 		} //if
 		return modelAndView;		
     }//SearchByYearSubmit
-	
 
+	@RequestMapping(value = "search/advanced", method = RequestMethod.GET)
+    public ModelAndView AdvancedSearchForm() {		
+		ModelAndView modelAndView = new ModelAndView();	
+		FormModel form = new FormModel();
+		modelAndView.addObject("form", form);
+		modelAndView.setViewName("/components/movie/advancedSearch");
+		return modelAndView;
+    }//AdvancedSearchForm
+
+	@RequestMapping(value="search/advanced", method = RequestMethod.POST)
+	public ModelAndView AdvancedSearchSubmit(@Valid @ModelAttribute("form") FormModel form, BindingResult result, 
+			 @RequestParam(value="searchType", required=true) String searchType, Model model){
+		
+		ModelAndView modelAndView = new ModelAndView();
+		List<MovieModel> movies = null;
+		
+		switch(searchType) {
+			case "title":
+				movies = movieService.findByTitleContainingIgnoreCaseOrderByTitleAsc(form.getTitle());
+	            break;
+	        case "director":
+	        	movies = movieService.findByDirectorContainingIgnoreCaseOrderByTitleAsc(form.getDirector());
+	            break;
+	        case "rating":
+	        	movies = movieService.findByRatingGreaterThanEqualOrderByTitleAsc(form.getRating());
+	            break;
+	        default:
+	        	modelAndView.setViewName("/components/movie/advancedSearch");
+	        	return modelAndView;
+		}//switch
+		
+		if(movies.isEmpty()){
+			modelAndView.setViewName("/components/movie/notFound");
+		} else if(result.hasErrors()){
+			modelAndView.setViewName("/components/movie/advancedSearch");
+		} else {
+			modelAndView.addObject("successMessage", "There is at least one movie of this year.");
+			model.addAttribute("movies", movies);
+			modelAndView.setViewName("/components/movie/result");
+		} //if
+		
+		return modelAndView;		
+    }//SearchByYearSubmit
+	
 }//class
