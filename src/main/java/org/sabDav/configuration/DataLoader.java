1b9a67d32252f91c483e256be7f3b0c3beeb27f7
David Ortega Arcos <DavidOrtega@users.noreply.github.com>
2017-06-06 21:34:31 +0200

    Revert "Persistencia en base de datos"

--- src/main/java/org/sabDav/configuration/DataLoader.java
+++ src/main/java/org/sabDav/configuration/DataLoader.java
@@ -24,29 +24,27 @@ public class DataLoader {
 	@PostConstruct
 	private void initDatabase() {
 		
-		if (userRepository.findByUsername("admin") == null) {
-			GrantedAuthority[] adminRoles = { new SimpleGrantedAuthority( "ROLE_USER"),
-			new SimpleGrantedAuthority( "ROLE_ADMIN") };
-			GrantedAuthority[] userRoles = { new SimpleGrantedAuthority( "ROLE_USER")};
-			userRepository.save(new UserModel("admin", "admin", "admin@email.com", Arrays.asList(adminRoles)));
-			userRepository.save(new UserModel("sabrina", "sabrina", "sabrina@email.com", Arrays.asList(userRoles)));
-			userRepository.save(new UserModel("david", "david", "david@email.com", Arrays.asList(userRoles)));
-			
-			MovieModel movie1 = new MovieModel("Inception");
-			movie1.setUrl_movie("https://www.youtube.com/embed/8hP9D6kZseM");
-			movie1.setUrl_cover("http://www.impawards.com/2010/posters/inception_xlg.jpg");
-			movie1.setDirector("Christopher Nolan");
-			movie1.setYear(2010);
-			movie1.setRating(8);
-			MovieModel movie2 = new MovieModel("Avatar");		
-			movie2.setUrl_movie("https://www.youtube.com/embed/tLylzuf1MWA");
-			MovieModel movie3 = new MovieModel("The Dark Knight");
-			movie3.setUrl_movie("https://www.youtube.com/embed/EXeTwQWrcwY");
-			movieRepository.save(movie1);
-			movieRepository.save(movie2);
-			movieRepository.save(movie3);
-		}// if repository not initialized
+		GrantedAuthority[] adminRoles = { new SimpleGrantedAuthority( "ROLE_USER"),
+		new SimpleGrantedAuthority( "ROLE_ADMIN") };
+		GrantedAuthority[] userRoles = { new SimpleGrantedAuthority( "ROLE_USER")};
+		userRepository.save(new UserModel("admin", "admin", "admin@email.com", Arrays.asList(adminRoles)));
+		userRepository.save(new UserModel("sabrina", "sabrina", "sabrina@email.com", Arrays.asList(userRoles)));
+		userRepository.save(new UserModel("david", "david", "david@email.com", Arrays.asList(userRoles)));
 		
-	}//initDatabase
+		MovieModel movie1 = new MovieModel("Inception");
+		movie1.setUrl_movie("https://www.youtube.com/embed/8hP9D6kZseM");
+		movie1.setUrl_cover("http://www.impawards.com/2010/posters/inception_xlg.jpg");
+		movie1.setDirector("Christopher Nolan");
+		movie1.setYear(2010);
+		movie1.setRating(8);
+		MovieModel movie2 = new MovieModel("Avatar");		
+		movie2.setUrl_movie("https://www.youtube.com/embed/tLylzuf1MWA");
+		MovieModel movie3 = new MovieModel("The Dark Knight");
+		movie3.setUrl_movie("https://www.youtube.com/embed/EXeTwQWrcwY");
+		movieRepository.save(movie1);
+		movieRepository.save(movie2);
+		movieRepository.save(movie3);
+		
+		}//initDatabase
 		
 }
