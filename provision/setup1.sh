f48b7dc22cc60f4145cbfdbda2eb2ea3f5dca6bb
Sabrina Fernández <sabrina.fer.zam@gmail.com>
2017-06-09 01:55:32 +0200

    BBDD con persistencia

--- provision/setup1.sh
+++ provision/setup1.sh
@@ -1,6 +1,24 @@
 echo "Installing Tomcat.."
 sudo apt-get install -y tomcat7
 
+echo "Install mysql client and server"
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+apt-get -y install mysql-server
+apt-get -y install mysql-client
+
+echo "configuring mysql networking"
+sudo cp /vagrant/provision/my.cnf /etc/mysql/my.cnf
+service mysql restart
+
+echo "creating mysql user"
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO root@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;"
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO root@'%'         IDENTIFIED BY 'root' WITH GRANT OPTION;"
+
+echo "create iRentMovies Database"
+mysql -uroot -proot < /vagrant/provision/db.sql
+
+
 echo "Copy file to Tomcat server"
 sudo cp /vagrant/target/iRentMovies.war /var/lib/tomcat7/webapps/
 
--- provision/setup2.sh
+++ provision/setup2.sh
@@ -1,6 +1,24 @@
 echo "Installing Tomcat.."
 sudo apt-get install -y tomcat7
 
+echo "Install mysql client and server"
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password root'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password root'
+apt-get -y install mysql-server
+apt-get -y install mysql-client
+
+echo "configuring mysql networking"
+sudo cp /vagrant/provision/my.cnf /etc/mysql/my.cnf
+service mysql restart
+
+echo "creating mysql user"
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO root@'localhost' IDENTIFIED BY 'root' WITH GRANT OPTION;"
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO root@'%'         IDENTIFIED BY 'root' WITH GRANT OPTION;"
+
+echo "create iRentMovies Database"
+mysql -uroot -proot < /vagrant/provision/db.sql
+
+
 echo "Copy file to Tomcat server"
 sudo cp /vagrant/target/iRentMovies.war /var/lib/tomcat7/webapps/
 
@@ -10,4 +28,4 @@ sudo cp /vagrant/target/iRentMovies.war /var/lib/tomcat7/webapps/
 echo "Restart Tomcat"
 sudo service tomcat7 restart
 
-echo "Go to the app: http://192.168.33.11:8081/iRentMovies/"
+echo "Go to the app: http://192.168.33.11:8080/iRentMovies/"
